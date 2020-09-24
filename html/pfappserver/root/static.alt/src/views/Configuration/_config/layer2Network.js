ae69c98237d90255f5c551836d2dbab233b985e4
James Rouzier <jrouzier@inverse.ca>
2020-09-24 07:43:27 -0400

    Add description field to the form

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/layer2Network.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/layer2Network.js
@@ -120,6 +120,16 @@ export const view = (form = {}, meta = {}) => {
           ]
         },
         {
+          label: i18n.t('Description'),
+          cols: [
+            {
+              namespace: 'description',
+              component: pfFormInput,
+              attrs: attributesFromMeta(meta, 'description')
+            }
+          ]
+        },
+        {
           label: i18n.t('Algorithm'),
           cols: [
             {
